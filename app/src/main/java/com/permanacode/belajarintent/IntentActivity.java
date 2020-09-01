4b418a78d5adda0f7fb7ec5cff492d0687fc95fe
milyardinardin <labanamilyardin@gmail.com>
2020-09-01 22:31:44 +0700

    BelajarIntent

---
+++ app/src/main/java/com/permanacode/belajarintent/IntentActivity.java
@@ -0,0 +1,14 @@
+package com.permanacode.belajarintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class IntentActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+
+    }
+}
